ac254a8149a8f950a732526edab0df98ba29e4ef
gary <gary.white@lodgit.com>
2022-05-09 13:43:22 +0200

    styling

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ function midiOctave(octArr) {
     for (var i = 0; i < octArr.length; i++) {
         myArr.push(octArr[i] + 12);
     }
-    return myArr;
+    console.log(myArr);
 }
 
-console.log(midiOctave([48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]));
+function myFunc() {
+    alert("You Clicked me");
+}
+
+//console.log(midiOctave([48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]));
 // return [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71]
